9252d0bd05303cf44b1678b75d295331fa74f1c3
hdlguy <hdlguy@hotmail.com>
2017-10-23 17:05:23 -0600

    Played with propagating HLS changes into my Verilog simulation. It's tricky. I found recreating the simulation project from scratch caused HLS changes to be reflected in simulation.

--- fpga/sim/setup.tcl
+++ fpga/sim/setup.tcl
@@ -3,16 +3,18 @@ close_project -quiet
 file delete -force proj.xpr *.os *.jou *.log proj.srcs proj.cache proj.runs
 #
 create_project -force proj 
-set_property board_part em.avnet.com:zed:part0:1.3 [current_project]
+#set_property board_part em.avnet.com:zed:part0:1.3 [current_project]
+set_property part xc7z020clg484-1 [current_project]
 set_property target_language Verilog [current_project]
 set_property default_lib work [current_project]
 load_features ipintegrator
 
-read_ip ../sin_cos_rom/sin_cos_rom.xci
+set_property  ip_repo_paths  ../../random_phase_modulator/solution1/impl/ip [current_project]
+read_ip ../source/phase_mod_0/phase_mod_0.xci
 upgrade_ip -quiet  [get_ips *]
 generate_target {all} [get_ips *]
 
 # Read in the hdl source.
-read_verilog -sv  [glob ../phase_mod.sv]
+read_verilog -sv  [glob ../source/phase_mod_tb.sv]
 
 close_project
