35b1638dcd6d746bfc4c58a326fd301508c886b4
pedro <hdlguy@hotmail.com>
2017-11-18 11:21:05 -0700

    complex_conv.cpp is correct in csim

--- HLS/complex_conv/source/complex_conv.cpp
+++ HLS/complex_conv/source/complex_conv.cpp
@@ -1,25 +1,27 @@
 // complex_conv.cpp
+#include <iostream>
 #include "complex_conv.hpp"
 
-bb_cplx_type complex_conv( bb_cplx_type x_in, coef_cplx_type (&coef)[num_coefs] )
+bb_cplx_type complex_conv( bb_cplx_type x_in, coef_cplx_type coef[num_coefs] )
 {
+#pragma HLS ARRAY_PARTITION variable=coef complete dim=1
 #pragma HLS STREAM variable=x_in dim=1
-#pragma HLS STREAM variable=coef dim=1
 #pragma HLS PIPELINE II=4
 
     // Implement the data delay pipeline.
-    bb_cplx_type x_delay[num_coefs];
-    for(int i=0; i<num_coefs-1; i++){
-        x_delay[i+1] = x_delay[i];
+	static bb_cplx_type x_delay[num_coefs];
+    for(int i=num_coefs-1; i>0; i--) {
+    	x_delay[i] = x_delay[i-1];
     }
     x_delay[0] = x_in;
 
-    // Implement the convolution.
-	bb_cplx_type conv_out = 0.0;
+    // Implement the multiplications.
+	bb_cplx_type conv_out = bb_cplx_type(0.0, 0.0);
     for(int i=0; i<num_coefs; i++){
-	    conv_out += x_delay[i] * coef[i];
+	    conv_out += x_delay[i] * coef[num_coefs-1-i];
     }
 
 	return(conv_out);
 }
+
 
--- HLS/complex_conv/source/complex_conv.hpp
+++ HLS/complex_conv/source/complex_conv.hpp
@@ -2,16 +2,17 @@
 #include <ap_fixed.h>
 #include <complex>
 
-const int bb_width   = 16;
+const int bb_width   = 18;
 const int coef_width = 16;
 const int num_coefs  = 32;
 
-typedef ap_fixed<bb_width, 1, AP_RND, AP_SAT, 2> bb_type;
+typedef ap_fixed<bb_width, 3, AP_RND, AP_SAT, 2> bb_type;
 typedef std::complex<bb_type> bb_cplx_type;
 
 typedef ap_fixed<coef_width, 1, AP_RND, AP_SAT, 2> coef_type;
 typedef std::complex<bb_type> coef_cplx_type;
 
-bb_cplx_type complex_conv( bb_cplx_type x_in, coef_cplx_type (&coef)[num_coefs] );
+bb_cplx_type complex_conv( bb_cplx_type x_in, coef_cplx_type coef[num_coefs] );
+
 
 
