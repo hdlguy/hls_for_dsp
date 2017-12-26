18d55b5afd0266b43624f8674263ac41edb447ce
pedro <pete@hdlguy.com>
2017-12-25 20:04:21 -0700

    sim setup script

---
+++ fpga/source/fader/sim/setup.tcl
@@ -0,0 +1,21 @@
+# This script sets up a Vivado project with all ip references resolved.
+close_project -quiet
+file delete -force proj.xpr *.os *.jou *.log proj.srcs proj.cache proj.runs
+#
+create_project -force proj 
+#set_property board_part em.avnet.com:zed:part0:1.3 [current_project]
+set_property part xc7z020clg484-1 [current_project]
+set_property target_language Verilog [current_project]
+set_property default_lib work [current_project]
+#load_features ipintegrator
+
+#set_property  ip_repo_paths  ../../HLS/phase_mod/csynth/solution1/impl/ip [current_project]
+#read_ip ../source/phase_mod_0/phase_mod_0.xci
+#upgrade_ip -quiet  [get_ips *]
+#generate_target {all} [get_ips *]
+
+# Read in the hdl source.
+read_verilog -sv  [glob ../fader.sv]
+read_verilog -sv  [glob ../fader_tb.sv]
+
+close_project
