a28496cb617a5a47ccde448176556ae458b772f6
pedro <pete@hdlguy.com>
2017-12-25 20:44:17 -0700

    the autogenerated states structure for Verilog looks right.

--- octave/run_fade.m
+++ octave/run_fade.m
@@ -1,5 +1,5 @@
 clear;
-N=4;     % number of independent fader channels.
+N=32;     % number of independent fader channels.
 M=8;     % number of reflectors per fade.
 T=1;     % simulation run time in seconds.
 Fs=10e3; % sampling rate of fade calculations in Hz.
@@ -12,7 +12,7 @@ states.theta = 2*pi*rand(N,1,1) - pi;
 %write_fader_hpp(states, fd, 'fader_states.hpp');
 write_verilog(states, fd, 'states_pack.sv');
 
-t=(0:1/Fs:T);  % time vector.
+t = (0:1/Fs:T);  % time vector.
 [X,states] = jakes_fader(t,fd,N,states,M);
 
 plot(t,20*log10(abs(X)));
--- octave/write_verilog.m
+++ octave/write_verilog.m
@@ -7,7 +7,7 @@ function [retval] = write_verilog (states, fd, filename)
     B_phi = 14; % bits in phi word.
     A_phi = 2^(B_phi-1);
     B_cos = 18; % bits in wd_cos_alpha word.
-    A_cos = 2^(B_cos-1);
+    A_cos = 2^(B_cos-2);
 
     N = size(states.theta, 1);
     M = size(states.Phi, 3);
@@ -22,18 +22,19 @@ function [retval] = write_verilog (states, fd, filename)
     fprintf(fp, '        logic   [%d-1:0][%d-1:0]   phi_real;\n', M, B_phi);
     fprintf(fp, '        logic   [%d-1:0][%d-1:0]   wd_sin_alpha;\n', M, B_cos);
     fprintf(fp, '        logic   [%d-1:0][%d-1:0]   wd_cos_alpha;\n', M, B_cos);
-    fprintf(fp, '    } fade_struct; \n');
+    fprintf(fp, '    } fade_struct; \n\n');
     
-    fprintf(fp, '    const state_array state[%d] = {\n', N);
+    fprintf(fp, '    const fade_struct state[%d] = {\n', N);
     
     wd_cos_alpha = zeros(1,M);
     wd_sin_alpha = zeros(1,M);
+
     for i=1:N
-        fprintf(fp, '    // channel = %d\n', i-1);
-        fprintf(fp, '    {\n');
-        fprintf(fp, '        {');
+        fprintf(fp, '        // channel = %d\n', i-1);
+        fprintf(fp, '        {\n');
+        fprintf(fp, '            {');
         for j=1:M
-            fprintf(fp, '%f', states.Phi(i,1,j));
+            fprintf(fp, '%d',   round(A_phi*states.Phi(i,1,j)/pi)   );
             if (j!=M) 
                 fprintf(fp, ', ');
             else
@@ -41,9 +42,9 @@ function [retval] = write_verilog (states, fd, filename)
             end
         end
         
-        fprintf(fp, '        {');
+        fprintf(fp, '            {');
         for j=1:M
-            fprintf(fp, '%f', states.phi(i,1,j));
+            fprintf(fp, '%d',   round(A_phi*states.phi(i,1,j)/pi)   );
             if (j!=M) 
                 fprintf(fp, ', ');
             else
@@ -51,16 +52,16 @@ function [retval] = write_verilog (states, fd, filename)
             end
         end
     
-        % we need to precompute the cos(alpha) and sin(alpha).
+        % we need to precompute the Wd*cos(alpha) and Wd*sin(alpha).
         for j=1:M
             alpha = ( 2*pi*j - pi + states.theta(i,1,1) )/(4*M);
-            wd_cos_alpha(j) = 2*pi*fd*cos(alpha);
-            wd_sin_alpha(j) = 2*pi*fd*sin(alpha);
+            wd_cos_alpha(j) = 2*pi*cos(alpha);
+            wd_sin_alpha(j) = 2*pi*sin(alpha);
         end
         
-        fprintf(fp, '        {');
+        fprintf(fp, '            {');
         for j=1:M
-            fprintf(fp, '%f', wd_cos_alpha(j));
+            fprintf(fp, '%d', round(A_cos*wd_cos_alpha(j)/pi)  );
             if (j!=M) 
                 fprintf(fp, ', ');
             else
@@ -68,9 +69,9 @@ function [retval] = write_verilog (states, fd, filename)
             end
         end
         
-        fprintf(fp, '        {');
+        fprintf(fp, '            {');
         for j=1:M
-            fprintf(fp, '%f', wd_sin_alpha(j));
+            fprintf(fp, '%d', round(A_cos*wd_sin_alpha(j)/pi)  );
             if (j!=M) 
                 fprintf(fp, ', ');
             else
@@ -79,32 +80,17 @@ function [retval] = write_verilog (states, fd, filename)
         end
         
         if (i!=N)
-            fprintf(fp, '    },\n');
+            fprintf(fp, '        },\n');
         else
-            fprintf(fp, '    }\n');
+            fprintf(fp, '        }\n');
         end
     end
     
-    fprintf(fp, '};\n');
-    
+    fprintf(fp, '    };\n\n');
+    fprintf(fp, 'endpackage\n\n');
     
     fclose(fp);
 
 endfunction
 
-%        for(int i=0; i<M; i++){
-%            theta = 2.0*M_PI*(((double)rand())/rand_max) - M_PI; // uniformly random over [-M_PI, M_PI).
-%            alpha = (2.0*M_PI*(i+1) - M_PI + theta)/(4.0*M);
-%            std::cout << j << ", " << i << " alpha = " << alpha << ", theta = " << theta << "\n";
-%            state[j].cos_alpha[i]    = (fade_type)cos(alpha); // [-1.0, +1.0]
-%            state[j].sin_alpha[i]    = (fade_type)sin(alpha);
-
-
-
-%typedef struct {
-%    fade_type phi_real[M];
-%    fade_type phi_imag[M];
-%    fade_type cos_alpha[M];
-%    fade_type sin_alpha[M];
-%} rand_state;
 
