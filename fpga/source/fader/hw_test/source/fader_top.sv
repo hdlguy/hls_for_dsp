e2d585a54e6286c99c565359450ece8a1133abc3
pedro <pete@hdlguy.com>
2018-01-12 20:54:58 -0700

    wiring convolver into hw_test pipeline

--- fpga/source/fader/hw_test/source/fader_top.sv
+++ fpga/source/fader/hw_test/source/fader_top.sv
@@ -164,89 +164,24 @@ module fader_top(
 
     linterp_ila linterp_ila_i ( .clk( clk ), .probe0( {linterp_dout_imag[1], linterp_dout_real[1], linterp_dout_imag[0], linterp_dout_real[0]}) );
 
+    always_comb begin
+        for(int i=0; i<32; i++) begin
+            coef_imag[i] = {linterp_dout_imag[i], 2'b00};
+            coef_real[i] = {linterp_dout_real[i], 2'b00};
+        end
+    end
+
+    logic [31:0][17:0] coef_imag, coef_real;
+    complex_conv_wrap conv_wrap_i (
+        .reset(reset),
+        .clk(clk),
+        .imag_in(0),
+        .real_in(0),
+        .imag_out(),
+        .real_out(),
+        .coef_imag(coef_imag),
+        .coef_real(coef_real)
+    ):
+
 endmodule
 
-/*
-
-    logic aresetn;
-    logic [15 : 0] s_axis_config_tdata;
-    logic s_axis_config_tvalid;
-    logic s_axis_config_tready;
-    logic [31 : 0] s_axis_data_tdata;
-    logic s_axis_data_tvalid;
-    logic s_axis_data_tready;
-    logic s_axis_data_tlast;
-    logic [31 : 0] m_axis_data_tdata;
-    logic [7 : 0] m_axis_data_tuser;
-    logic m_axis_data_tvalid;
-    logic m_axis_data_tready;
-    logic m_axis_data_tlast;
-    logic event_frame_started;
-    logic event_tlast_unexpected;
-    logic event_tlast_missing;
-    logic event_status_channel_halt;
-    logic event_data_in_channel_halt;
-    logic event_data_out_channel_halt;
-
-    fade_ifft uut (
-        .aclk(clk),                                              
-        .aresetn(aresetn), 
-        //                                      
-        .s_axis_config_tdata(s_axis_config_tdata),               
-        .s_axis_config_tvalid(s_axis_config_tvalid),             
-        .s_axis_config_tready(s_axis_config_tready),             
-        //
-        .s_axis_data_tdata(s_axis_data_tdata),                   
-        .s_axis_data_tvalid(s_axis_data_tvalid),                 
-        .s_axis_data_tready(s_axis_data_tready),                 
-        .s_axis_data_tlast(s_axis_data_tlast),
-        //                   
-        .m_axis_data_tdata(m_axis_data_tdata),                   
-        .m_axis_data_tuser(m_axis_data_tuser),                   
-        .m_axis_data_tvalid(m_axis_data_tvalid),                 
-        .m_axis_data_tready(m_axis_data_tready),                 
-        .m_axis_data_tlast(m_axis_data_tlast),
-        //                   
-        .event_frame_started(event_frame_started),               
-        .event_tlast_unexpected(event_tlast_unexpected),         
-        .event_tlast_missing(event_tlast_missing),               
-        .event_status_channel_halt(event_status_channel_halt),   
-        .event_data_in_channel_halt(event_data_in_channel_halt), 
-        .event_data_out_channel_halt(event_data_out_channel_halt)
-    );
-
-    logic win_dv_in;
-    assign win_dv_in = m_axis_data_tvalid;
-    logic [4:0] win_index_in, win_index_out;
-    assign win_index_in = m_axis_data_tuser[4:0];
-    logic [16-1:0] win_din_imag, win_din_real;
-    assign win_din_imag = m_axis_data_tdata[31:16];
-    assign win_din_real = m_axis_data_tdata[15:0];
-    logic  win_dv_out;
-    logic [16-1:0] win_dout_imag, win_dout_real;
-    win windower (
-        .clk(clk),
-        .dv_in(win_dv_in),
-        .index_in(win_index_in),
-        .din_imag(win_din_imag), 
-        .din_real(win_din_real),
-        .dv_out(win_dv_out),
-        .index_out(win_index_out),
-        .dout_imag(win_dout_imag),
-        .dout_real(win_dout_real)
-    );
-
-
-    logic linterp_dv_out;
-    logic [31:0][15:0] linterp_dout_real, linterp_dout_imag;
-    linterp interpolator(
-        .clk(clk),
-        .dv_in(win_dv_out),
-        .index_in(win_index_out),
-        .din_real(win_dout_real),
-        .din_imag(win_dout_imag),
-        .dout_imag(linterp_dout_imag),
-        .dout_real(linterp_dout_real)
-    );
-
-*/
