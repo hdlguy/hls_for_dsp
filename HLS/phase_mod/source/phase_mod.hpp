c8e33d62b5534b40ce24c023a917e136e12d5567
hdlguy <pedro@hdlguy.com>
2019-02-17 17:29:18 -0500

    miscellaneous

--- HLS/phase_mod/source/phase_mod.hpp
+++ HLS/phase_mod/source/phase_mod.hpp
@@ -4,8 +4,16 @@
 
 const int bb_width = 12;
 typedef ap_fixed<bb_width, 1, AP_RND, AP_SAT, 2> bb_type;
-
 typedef std::complex<bb_type> bb_cplx_type;
+
+const int ph_width = 13;
+typedef ap_fixed<ph_width, 4, AP_RND, AP_SAT, 2> ph_type;
+typedef std::complex<ph_type> ph_cplx_type;
+
+const int prod_width = 18;
+typedef ap_fixed<prod_width, 4, AP_RND, AP_SAT, 2> prod_type;
+typedef std::complex<prod_type> prod_cplx_type;
+
 
 bb_cplx_type phase_mod( bb_cplx_type bb_in, bb_cplx_type ph_in);
 
